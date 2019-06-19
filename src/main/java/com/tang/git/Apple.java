caa7bef6d1281cc729d0d96dd893f0bd2f5d8a16
tang_glb <tang_glb123@126.com>
2019-06-19 09:50:48 +0800

    update apple

--- src/main/java/com/tang/git/Apple.java
+++ src/main/java/com/tang/git/Apple.java
@@ -1,5 +1,15 @@
 package com.tang.git;
 
 public class Apple {
+	private String color;
 
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
 }
